211d0228ccefcafbc952a70d55a8037059496bd8
KanonKanon <164764418@qq.com>
2018-02-28 19:28:01 +0800

    Update nester.py

--- nester.py
+++ nester.py
@@ -1,12 +1,15 @@
 
 """ 这是一个打印列表中所有数据模块"""
 
-def print_all(list_obj,indent=0):
+def print_all(list_obj,isindent=False,indent=0):
 	""" list_obj参数是一个复杂的列表，这个列表是个多重嵌套的列表，
-	输出每一个数据到屏幕上，各数据项占一行 indent 表示要缩进的数量 默认为0"""
+	输出每一个数据到屏幕上，各数据项占一行,isindent 表示是否要缩进 indent 表示要缩进的数量 默认为0"""
 	for each in list_obj:
 		if isinstance(each,list):
-			print_all(each,indent+1)
+			if isindent:
+				print_all(each,True,indent+1)
+			else:
+				print_all(each)
 		else:
 			for i in range(indent):
 				print('\t',end='')
