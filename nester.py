d7d453cd55abfbac79dc73d399ae608540dd5acf
KanonKanon <164764418@qq.com>
2020-04-12 21:26:18 +0800

    测试

--- nester.py
+++ nester.py
@@ -1,11 +1,15 @@
 
+
 """ 这是一个打印列表中所有数据的方法"""
 
-def print_all(list_obj):
+def print_all(list_obj，indent = False,level =0, fileoutput = sys.stdout):
 	""" 参数是一个复杂的列表，这个列表是个多重嵌套的列表"""
 	for each in list_obj:
 		if isinstance(each,list):
-			print_all(each)
+			print_all(each,indent,level+1,fileoutput)
 		else:
-			print(each)
+			if indent:
+				for tab_stop in range(level):
+					print("/t",end='',file = fileoutput)
+				print(each,fileoutput)
 
